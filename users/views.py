26109b0aa62cd8392db72833c4c1642d4fa6321a
Kodim-program <kodim.pervomaisk@gmail.com>
2026-07-30 10:20:44 +0300

    adaptive material curse

--- users/views.py
+++ users/views.py
@@ -35,3 +35,25 @@ def profile(request):
         'recent_progress': recent_progress,
     }
     return render(request, 'registration/profile.html', context)
+
+
+class Register(View):
+    template_name = 'registration/registration.html'
+
+    def get(self, request):
+        context = {
+            'form': UserRegisterForm()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, self.template_name, {'form': form})
